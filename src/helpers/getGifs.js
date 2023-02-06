7dbb70990148da3803e040c48ea9e6316cf1d1ae
4nacs4 <4nacs4@gmail.com>
2023-02-06 15:42:27 -0500

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+
+export const getGifts = async ( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=Pao3MRs7GmuDF5UeJfGZiEXYeLvgvKZE&q=${ category }&limit=20`;
+    const resp = await fetch( url );
+    const { data } =  await resp.json();
+    const gifts =  data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+
+    }));
+    console.log(gifts);
+    return gifts;
+}
